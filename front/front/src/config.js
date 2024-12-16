aa08dfe1db25aebd0ecdf16b6fd4d7d9ad5e8d85
Exfis <119972923+Exfis1@users.noreply.github.com>
2024-12-16 12:32:18 +0200

    Update config.js

--- front/front/src/config.js
+++ front/front/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "http://localhost:5037/api/v1"; // Update with your backend URL if hosted elsewhere
+const API_BASE_URL = "https://seal-app-7mgwr.ondigitalocean.app/api/v1"; // Update with your backend URL if hosted elsewhere
 export default API_BASE_URL;
