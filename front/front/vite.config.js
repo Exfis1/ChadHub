2f7c1535e782648392fa52d253391d4db9c55afe
Exfis <119972923+Exfis1@users.noreply.github.com>
2024-12-16 12:46:55 +0200

    Update vite.config.js

--- front/front/vite.config.js
+++ front/front/vite.config.js
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		extensions: ['.mjs', '.js', '.jsx', '.json'], // Ensure .js is included for resolution
-	},
-})
+    base: './',
+    plugins: [react()],
+    resolve: {
+        extensions: ['.mjs', '.js', '.jsx', '.json'],
+    },
+    build: {
+        rollupOptions: {
+            external: ['react-router-dom'], // Externalize dependency if needed
+        },
+    },
+});
