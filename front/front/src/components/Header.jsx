b1afa0aa2c422f8554c0036b465f0e848b7546b3
Exfis <119972923+Exfis1@users.noreply.github.com>
2024-12-16 12:59:36 +0200

    Update Header.jsx

--- front/front/src/components/Header.jsx
+++ front/front/src/components/Header.jsx
@@ -1,7 +1,7 @@
 ﻿import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-import logo from "../assets/chadhub.png";
+import logo from "../assets/ChadHub.png";
 
 const Header = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
