efc653204183783909bcc489ec09fb98b58d5b9c
Exfis <119972923+Exfis1@users.noreply.github.com>
2024-12-16 13:23:16 +0200

    CORS update

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
+        policy.WithOrigins("https://coral-app-byyra.ondigitalocean.app").AllowAnyHeader().AllowAnyMethod();
     });
 });
 
